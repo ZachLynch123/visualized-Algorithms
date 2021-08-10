53ed669edb11aeabaad42f20203cad31aa06629a
Zach Lynch <zachary.lynch1337@gmail.com>
2021-08-10 10:25:34 -0700

    sorting algorithm works

--- bubbleSort/sketch.js
+++ bubbleSort/sketch.js
@@ -4,15 +4,41 @@ let values = [];
 function setup() {
   createCanvas(400, 400);
   values = [width];
-  // gets 400 random whole numbers 
-  for (let i = 1; i < values[0]; i++) {
-    values.push(Math.floor(Math.random() * 10))
+  // gets 400 random whole numbers 1 - height 
+  for (let i = 1; i < width; i++) {
+    values.push(Math.floor(Math.random() * (height - 1) + 1))
   }
+
+  bubbleSort();
+
+  // sorted yay.. values = sort(values)
 }
 
 function draw() {
   background(0);
   for (let i = 0; i < values.length; i++) {
-
+    stroke(255);
+    line(i, height, i, height - values[i]); // draws line with value at index i
   }
+}
+
+// 
+const bubbleSort = () => {
+  for (let i = 0; i < values.length; i++) {
+    for (let j = 0; j < values.length - 1; j++) {
+      let a = values[j];
+      let b = values[j + 1];
+      if (a > b) {
+        console.log("running");
+        swap(values, j, j + 1)
+      }
+    }
+  }
+}
+
+
+const swap = (values,a, b) => {
+  let tempA = values[a];
+  values[a] = values[b]
+  values[b] = tempA;
 }
